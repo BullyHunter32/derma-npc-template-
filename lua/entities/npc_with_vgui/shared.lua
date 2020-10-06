8e38cd77a949d6a4f0447b04106b47bc52a4f96a
BullyHunter32 <60613196+BullyHunter32@users.noreply.github.com>
2020-10-06 16:18:31 +0100

    Update shared.lua

--- lua/entities/npc_with_vgui/shared.lua
+++ lua/entities/npc_with_vgui/shared.lua
@@ -3,7 +3,7 @@ ENT.Type = "ai"
 ENT.PrintName = "BullyHunter - BaseNPC"
 ENT.Category = "Base NPC"
 ENT.Spawnable = true
-
+ENT.Admin = true -- makes the entity only spawnable by admins
 ENT.AutomaticFrameAdvance = true
 
 function ENT:SetAutomaticFrameAdvance( bUsingAnim )
